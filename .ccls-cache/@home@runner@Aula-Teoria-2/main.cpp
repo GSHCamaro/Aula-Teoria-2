020e8983e15b87e6f53355884953a2399ffce9e6
gcmrzn <20980358-GCamarozano@users.noreply.replit.com>
2024-08-22 00:52:15 +0000

    Usando Vários Namespace

--- .ccls-cache/@home@runner@Aula-Teoria-2/main.cpp
+++ .ccls-cache/@home@runner@Aula-Teoria-2/main.cpp
@@ -1,23 +1,24 @@
 #include <iostream>
+using namespace std;
 
-int x=1; // variavel global
-
-void f(){
-  // usa variavel global
-  std::cout << "dentro da funcao f: "<<x<<std::endl;
+namespace coords1{
+  int x=1;
+  int y=2;
 }
 
-void f2(int x){  
-  // parametro
-  std::cout << "dentro da funcao f2: "<<x<<std::endl;}
+namespace coords2{
+  int x=3;
+  int y=4;
+  int z = 6;
+}
 
-int main() { 
-  int x=2; // variavel local
-  std::cout <<"dentro da main: "<< x <<std::endl;
-  f();
-  f2(x);
-  {
-  int x = 3; // variavel dentro do bloco
-  std::cout <<"dentro do bloco na main: "<< x <<std::endl;
-  }
+int main() {
+  int z = 7;
+  // int x = 0;
+  // int y = 0;
+  std::cout << "x = " << coords1::x << std::endl;
+  std::cout << "y = " << coords2::y << std::endl;
+  std::cout << "z = " << z << endl;
+  // cout << "coords1::x = " << coords1::x << endl;
+  // cout << "coords1::x = " << coords1::y << endl;
 }
