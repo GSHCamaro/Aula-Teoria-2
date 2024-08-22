8621dd4a99b52ba62ca820caa12222c4bd6bac01
gcmrzn <20980358-GCamarozano@users.noreply.replit.com>
2024-08-22 00:51:28 +0000

    Initial commit

---
+++ .ccls-cache/@home@runner@Aula-Teoria-2/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+int x=1; // variavel global
+
+void f(){
+  // usa variavel global
+  std::cout << "dentro da funcao f: "<<x<<std::endl;
+}
+
+void f2(int x){  
+  // parametro
+  std::cout << "dentro da funcao f2: "<<x<<std::endl;}
+
+int main() { 
+  int x=2; // variavel local
+  std::cout <<"dentro da main: "<< x <<std::endl;
+  f();
+  f2(x);
+  {
+  int x = 3; // variavel dentro do bloco
+  std::cout <<"dentro do bloco na main: "<< x <<std::endl;
+  }
+}
